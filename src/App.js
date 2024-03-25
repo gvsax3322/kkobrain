98701f792efbc8fb08804c45fb76f395d5e9e871
gvsax3322 <cosbe3822@gmail.com>
2024-03-25 12:53:49 +0900

    리익트 적용하지

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./css/common/common.css";
+import "./css/normalize.css";
+import "./css/style.css";
+
+import Layout from "./layouts/Layouts";
+import Main from "./pages/Main";
+// 컴포넌트 (즉, JSX 뽑기)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrap">
+        <Layout>
+          <Main />
+        </Layout>
+      </div>
+    </>
   );
 }
 
